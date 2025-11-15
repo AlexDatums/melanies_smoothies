ba8410f9ec5a92624bbbe3c5b0f46c9e51e524d0
AlexDatums <alex.datums@gmail.com>
2025-11-14 23:15:33 -0500

    Update streamlit_app.py
    moved import up

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 #from snowflake.snowpark.functions import when_matched
+import requests
 
 # Write directly to the app
 st.title(  f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
@@ -48,6 +49,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order  + "!", icon="✅")
 
-import requests
+#import requests
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
